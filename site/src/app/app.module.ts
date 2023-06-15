a763fb53eca4d51ea93519f4f91bbe1a78d24986
Ivirson Daltro <ivirson.daltro@ibm.com>
2023-06-14 21:51:26 -0300

    Aula 01 - part 2

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, CarouselComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
