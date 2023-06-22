8bdc8113c5a19c4a1398f86769168de96edebdde
Ivirson Daltro <ivirson.daltro@ibm.com>
2023-06-21 22:00:12 -0300

    Aula 03 - part 2

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { ContactPayload } from './models/contact-payload.model';
 })
 export class AppComponent {
   title = 'site';
+  public userIsAuthenticated: boolean = false;
 
   public data: AppData = {
     features: {
@@ -42,7 +43,7 @@ export class AppComponent {
     },
     about: {
       sectionTitle: 'Sobre a empresa',
-      text: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dolor arcu, malesuada eget posuere et, aliquam non elit. Sed maximus ullamcorper dui, ac sollicitudin quam molestie ac. Aenean laoreet, felis id sollicitudin fringilla, leo orci iaculis eros, et volutpat nunc lacus ut sapien. Aliquam velit justo, vestibulum et augue ut, efficitur ultrices justo. Aenean non eros congue leo consectetur fermentum. Quisque ut dignissim tortor, eget porttitor magna. Etiam sem turpis, aliquam sit amet nisi non, bibendum finibus neque. Duis id odio dapibus, finibus tortor eget, cursus nunc. Morbi egestas nisl orci, in cursus ipsum cursus et. Pellentesque in suscipit ipsum, vel auctor nisl. Quisque vitae vehicula velit. Integer dolor sapien, rutrum faucibus eros ac, hendrerit aliquet diam. Quisque non sem eu odio vestibulum facilisis ac aliquam tellus. Cras aliquet velit quis suscipit tincidunt. Phasellus ut aliquam nulla. </p> <p> Nam dictum diam nibh, id ultricies nunc rhoncus at. Nullam lobortis, erat vitae tempus semper, ante leo elementum odio, eu viverra nisi est in odio. Sed mi quam, ultrices vel placerat a, finibus ut neque. Curabitur eu leo eleifend, congue nisi et, vestibulum ex. Nulla hendrerit ex non orci dictum mattis. Fusce faucibus vitae massa nec fermentum. Nam tristique libero ac tellus vestibulum bibendum. Phasellus euismod, metus ut pretium vulputate, mi orci dignissim enim, id egestas augue ex in neque. Nulla ac dictum arcu, sit amet fringilla nisi. Duis elementum blandit nisi et ultrices. Morbi scelerisque auctor dui, egestas varius tellus. Sed id placerat felis. </p>',
+      text: '<p>Lorem ipsum dolor sit amet, <strong>consectetur</strong> adipiscing elit. Ut dolor arcu, malesuada eget posuere et, aliquam non elit. Sed maximus ullamcorper dui, ac sollicitudin quam molestie ac. Aenean laoreet, felis id sollicitudin fringilla, leo orci iaculis eros, et volutpat nunc lacus ut sapien. Aliquam velit justo, vestibulum et augue ut, efficitur ultrices justo. Aenean non eros congue leo consectetur fermentum. Quisque ut dignissim tortor, eget porttitor magna. Etiam sem turpis, aliquam sit amet nisi non, bibendum finibus neque. Duis id odio dapibus, finibus tortor eget, cursus nunc. Morbi egestas nisl orci, in cursus ipsum cursus et. Pellentesque in suscipit ipsum, vel auctor nisl. Quisque vitae vehicula velit. Integer dolor sapien, rutrum faucibus eros ac, hendrerit aliquet diam. Quisque non sem eu odio vestibulum facilisis ac aliquam tellus. Cras aliquet velit quis suscipit tincidunt. Phasellus ut aliquam nulla. </p> <p> Nam dictum diam nibh, id ultricies nunc rhoncus at. Nullam lobortis, erat vitae tempus semper, ante leo elementum odio, eu viverra nisi est in odio. Sed mi quam, ultrices vel placerat a, finibus ut neque. Curabitur eu leo eleifend, congue nisi et, vestibulum ex. Nulla hendrerit ex non orci dictum mattis. Fusce faucibus vitae massa nec fermentum. Nam tristique libero ac tellus vestibulum bibendum. Phasellus euismod, metus ut pretium vulputate, mi orci dignissim enim, id egestas augue ex in neque. Nulla ac dictum arcu, sit amet fringilla nisi. Duis elementum blandit nisi et ultrices. Morbi scelerisque auctor dui, egestas varius tellus. Sed id placerat felis. </p>',
     },
     address: {
       sectionTitle: 'Endereço',
@@ -62,5 +63,11 @@ export class AppComponent {
 
   public handleFormData(event: ContactPayload) {
     console.log('Evento recebido pelo componente pai:', event);
+  }
+
+  public auth(event: boolean): void {
+    console.log('aqui');
+
+    this.userIsAuthenticated = event;
   }
 }
--- site/src/app/components/header/header.component.ts
+++ site/src/app/components/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  @Output() public authEmitter = new EventEmitter<boolean>();
 
+  public userIsAuthenticated: boolean = false;
+
+  public auth(): void {
+    this.userIsAuthenticated = !this.userIsAuthenticated;
+    this.authEmitter.emit(this.userIsAuthenticated);
+  }
 }
